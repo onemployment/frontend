4fa9ce96fe0915f81f3d7353c0fca575d198ca8f
Reza Gholizadeh <rzgholizadeh@gmail.com>
2026-03-13 16:26:04 -0700

    feat: update routes for /home with AppShell and nested routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
 import App from '../App';
-import Landing from '../pages/Landing';
 import LoginPage from '../pages/LoginPage';
 import RegisterPage from '../pages/RegisterPage';
+import AppShell from '../components/layout/AppShell';
+import HomePage from '../pages/HomePage';
+import ResumeBuilderPage from '../pages/ResumeBuilderPage';
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/app" element={<Landing />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<RegisterPage />} />
+      <Route path="/home" element={<AppShell />}>
+        <Route index element={<HomePage />} />
+        <Route path="resume-builder" element={<ResumeBuilderPage />} />
+      </Route>
       <Route path="*" element={<App />} />
     </Routes>
   );
